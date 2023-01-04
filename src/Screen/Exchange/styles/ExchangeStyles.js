a99d60f72980f9cda7789ea2955ec8a8cd74784a
Tran Tan Phuoc <trantanphuoc262@gmail.com>
2023-01-04 18:10:12 +0700

    IU Exchange

--- src/Screen/Exchange/styles/ExchangeStyles.js
+++ src/Screen/Exchange/styles/ExchangeStyles.js
@@ -5,6 +5,39 @@ const styles = StyleSheet.create({
         backgroundColor:'#fff',
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
     },
+    scrollview:{
+        marginLeft:10,
+        marginRight:10, 
+        marginBottom:80,
+     },
+     containerTop:{
+        marginTop:20,
+        height:40,
+        marginLeft:15,
+        marginRight:15,
+        backgroundColor:'#E6E6FA',
+        borderRadius:20,
+        flex:1,
+        display:'flex',
+        flexDirection:'row'
+     },
+     containerInputMoney:{
+        marginTop:20,
+        height:70,
+        marginLeft:15,
+        marginRight:15,
+        flex:1,
+        display:'flex',
+        flexDirection:'row',
+     },
+     containerMoneyWords:{
+        height:60,
+        marginLeft:15,
+        marginRight:15,
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+     }
 });
 
 export default styles;
