5d4ea4bebeb9f5841ea5ca92bab1642053cdd76e
Tran Tan Phuoc <trantanphuoc262@gmail.com>
2023-02-18 17:58:54 +0700

    XuLi 18_02_2023

---
+++ src/Screen/Chart/styles/ChartStyles.js
@@ -0,0 +1,118 @@
+import {Platform, StatusBar, StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#F5F5FA',
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
+    containerheader:{
+      height:50,
+      backgroundColor:'#fff',
+      display:'flex',
+      flexDirection:'row',
+    },
+    containerheader_icon:{
+      flex:0.2,
+      justifyContent:'center',
+      marginLeft:20,
+    },
+    containerheader_title:{
+      flex:0.6,
+      justifyContent:'center',
+      alignItems:'center'
+    },
+    fontTitle:{
+      fontSize:18,
+      fontWeight:'500'
+    },
+    viewBody:{
+      marginLeft:10,
+      marginRight:10,
+      marginTop:10,
+      flex:1,
+      marginBottom:10,
+      borderRadius:20,
+      backgroundColor:'#fff',
+      shadowOffset:{
+        width:0,
+        height:1,
+      },
+      shadowColor:'#999999',
+      shadowOpacity: 0.5,
+      shadowRadius: 2,
+   },
+   containerItem:{
+    height:80,
+    margin:5,
+    borderRadius:10,
+    alignItems:'center',
+    marginLeft:10,
+    marginRight:10,
+    display:'flex',
+    flexDirection:'row',
+    flex:1,
+    justifyContent:'space-between'
+   },
+   containerSelectDropDown:{
+    borderRadius:20,
+    borderWidth:0.5,
+    borderColor:'black',
+    height:40,
+    borderRadius:20,
+    backgroundColor:'#fff',
+    shadowOffset:{
+      width:0,
+      height:1,
+    },
+    shadowColor:'#999999',
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    flex:0.4,    
+   },
+   centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    backgroundColor: '#555555',
+    borderRadius: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  containerItemSelect:{
+    height:80,
+    margin:5,
+    borderRadius:10,
+    justifyContent:'center',
+    marginLeft:10,
+    marginRight:10,
+   },
+   buttom:{
+    borderRadius:20,
+    borderWidth:0.5,
+    borderColor:'black',
+    height:30,
+    borderRadius:20,
+    backgroundColor:'#fff',
+    shadowOffset:{
+      width:0,
+      height:1,
+    },
+    shadowColor:'#999999',
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    justifyContent:'center',
+    alignItems:'center',
+    width:150,
+   },
+});
+
+export default styles;
--- src/Screen/History/History.js
+++ src/Screen/History/History.js
@@ -1,27 +1,21 @@
 import { Button, DatePickerAndroid, ScrollView, View } from 'react-native';
 import {  Text, SafeAreaView, Alert, Image,} from 'react-native';
 import styles from "./styles/HistoryStyles";
-import { AntDesign, FontAwesome } from '@expo/vector-icons';
+import { AntDesign,  } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
-import { TextInput } from 'react-native';
 import { useEffect, useState } from 'react';
-import SelectDropdown from 'react-native-select-dropdown';
-import DateTimePicker from '@react-native-community/datetimepicker';
-import { Modal } from 'react-native';
 // Import FireBase
 import{initializeAuth,signInWithEmailAndPassword,} from 'firebase/auth';
 import {initializeApp} from 'firebase/app';
 import { firebaseConfig } from "../../../firebase/ConnectFirebase";
 import { colorJar } from '../../../assets/AppColors/AppColors';
-import { useDispatch, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import axios from 'axios';
 
 function History({navigation,route}){
    const {id,name} = route.params;
-   const dataTK = ['Tất cả','Theo tháng','Theo năm'];
-   const dataJar = ['Tất cả','Thu nhập','Chi tiêu'];
 
-const [dataHistory,setdataHistory] = useState([]);
+    const [dataHistory,setdataHistory] = useState([]);
     const moneyFormat = (amount) => {
         return amount.toLocaleString("vi-VN", {
           style: "currency",
@@ -29,24 +23,7 @@ const [dataHistory,setdataHistory] = useState([]);
           maximumFractionDigits: 3,
         });
     };
-    const [valuesDefaut,setvaluesDefaut] = useState("Tất cả");
-    const [selectDefaut,setselectDefaut] = useState("Tất cả");
     const idReload = useSelector(state => state.reload.idReload);
-    const [selectedDate, setSelectedDate] = useState(new Date());
-    const [showPicker, setShowPicker] = useState(false);
-    const [month,setMonth] = useState(0);
-    const [year,setYear] = useState(2023);
-    const onDateChange = (event, selectedDate) => {
-        if (selectedDate) {
-          const newDate = new Date(selectedDate);
-          setSelectedDate(newDate);
-        }
-      };
-    const hanldChon = ()=>{
-        setShowPicker(false);
-        setMonth(selectedDate.getMonth() + 1);
-        setYear(selectedDate.getFullYear());
-    }
     // Connect FireBase
     const app = initializeApp(firebaseConfig);
     const auth = initializeAuth(app,{
@@ -77,35 +54,6 @@ const [dataHistory,setdataHistory] = useState([]);
     },[idReload])
     return(
         <SafeAreaView style={styles.container} >
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={showPicker}
-                onRequestClose={() => {
-                Alert.alert('Modal has been closed.');
-                setModalVisible(!modalVisible);
-                }}>
-                    <View style={styles.centeredView}>
-                        <View style={styles.modalView}>
-                            <View style={{display:'flex',flexDirection:'row',justifyContent:'center'}}>
-                                <View style={{justifyContent:'flex-start',flex:1,marginLeft:30,height:30}}>
-
-                                </View>
-                                <View style={{justifyContent:'flex-end',flex:1,marginRight:30,}}>
-                                    <TouchableOpacity onPress={hanldChon}>
-                                        <Text style={{fontSize:24,color:'#0099FF',textAlign:'right',fontWeight:'normal'}}>Chọn</Text>
-                                    </TouchableOpacity>
-                                </View>
-                            </View>
-                            <DateTimePicker
-                                value={selectedDate}
-                                mode="date"
-                                display="spinner"
-                                onChange={onDateChange}
-                            />
-                        </View>
-                    </View>
-            </Modal>
             <View style={styles.containerheader}>
                 <View style={styles.containerheader_icon}>
                     <TouchableOpacity onPress={()=>{
@@ -124,58 +72,6 @@ const [dataHistory,setdataHistory] = useState([]);
                 </View>
             </View>
             <ScrollView style={styles.viewBody}>
-                <View style={styles.containerItem}>
-                        <SelectDropdown 
-                            data={dataTK} 
-                            defaultButtonText={valuesDefaut} 
-                            buttonTextStyle = {{fontSize:16,}}
-                            onSelect={(selectedItem, index) => { 
-                                setvaluesDefaut(selectedItem);
-                            }} 
-                            renderDropdownIcon={isOpened => {
-                            return <AntDesign name={isOpened ? 'filter' : 'filter'} color={'black'} size={20} />;
-                            }}
-                            renderCustomizedButtonChild= {value =>{
-                                return (
-                                    <View style={{ flexDirection: 'row',alignItems:'center',display:'flex',justifyContent:"center"}}>
-                                            <Text style={{fontSize:20}}>{valuesDefaut}</Text>
-                                    </View>
-                                );
-                            }}
-                            buttonStyle = {styles.containerSelectDropDown}
-                        />
-
-                        <SelectDropdown 
-                        data={dataJar} 
-                        defaultButtonText={selectDefaut} 
-                        buttonTextStyle = {{fontSize:16,}}
-                        onSelect={(selectedItem, index) => { 
-                            setselectDefaut(selectedItem);
-                        }} 
-                        renderDropdownIcon={isOpened => {
-                        return <FontAwesome name={isOpened ? 'chevron-down' : 'chevron-right'} color={'black'} size={16} />;
-                        }}
-                        renderCustomizedButtonChild= {value =>{
-                            return (
-                                <View style={{ flexDirection: 'row',alignItems:'center',display:'flex',justifyContent:"center"}}>
-                                        <Text style={{fontSize:20}}>{selectDefaut}</Text>
-                                </View>
-                            );
-                        }}
-                        buttonStyle = {styles.containerSelectDropDown}
-                    />
-                </View>
-                {
-                    (valuesDefaut == "Theo tháng" || valuesDefaut == "Theo năm" ) &&
-                    <View style={styles.containerItemSelect}>
-                        <TouchableOpacity style={styles.buttom} onPress={() => setShowPicker(true)} >
-                                <Text>
-                                    Chọn tháng
-                                </Text>
-                        </TouchableOpacity>
-                        <Text style={{fontSize:20,fontWeight:'bold',marginTop:10,}}>Tháng: {selectedDate.toLocaleDateString('VN', { month: 'long', year: 'numeric' })}</Text>
-                    </View>
-                }
                 {
                     dataHistory.map((item,index)=>{
                         return(
